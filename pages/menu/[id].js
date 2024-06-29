7cf09ddd8e313013369218fe549a01f45af1b789
saeed joulaei <saeedjoulaei86@gmail.com>
2024-06-29 13:58:37 +0330

    [id].js page is completed

---
+++ pages/menu/[id].js
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+
+function Details({ data }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading ...</div>;
+  }
+  return <div>{data.id}</div>;
+}
+
+export default Details;
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:4000/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+
+  const paths = data.map((user) => ({
+    params: {
+      id: user.id.toString(),
+    },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  console.log(params);
+  const res = await fetch(`http://localhost:4000/data/${params.id}`);
+  const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+    revalidate: 10,
+  };
+}
